780c0b39029087e9af7bf8113a029cbb61f1541a
Raj Bhatia <50281419+raj2784@users.noreply.github.com>
2021-10-21 15:39:54 +1030

    first commit

---
+++ CandidateAPI/Controllers/CandidateController.cs
@@ -0,0 +1,55 @@
+﻿using CandidateAPI.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CandidateAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidateController : ControllerBase
+    {
+        private readonly AppDbContext db;
+
+        public CandidateController(AppDbContext context)
+        {
+           db = context;
+        }
+        // GET: api/<CandidateController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CandidateModel>>> GetAllCndidates()
+        {
+            return await db.Candidates.ToListAsync();
+        }
+
+
+        // GET api/<CandidateController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CandidateModel>> GetCandidate(int id)
+        {
+            var candidate = await db.Candidates.FindAsync(id);
+            if(candidate !=null)
+            {
+                return candidate;
+            }
+            return NotFound();
+        }
+
+        // POST api/<CandidateController>
+        [HttpPost]
+        public async Task<ActionResult<CandidateModel>> PostCandidate(CandidateModel model)
+        {
+            db.Candidates.Add(model);
+            await db.SaveChangesAsync();
+            return CreatedAtAction("GetCandidate", new { Id = model.Id, model });
+
+        }
+
+       
+    }
+}
---
+++ CandidateAPI/Migrations/20211021023123_Initial-Migration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CandidateAPI.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Candidates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Candidates", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Candidates");
+        }
+    }
+}
